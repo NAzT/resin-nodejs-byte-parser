8db1952636d97d5a462c3dc8572e3df38c9b55c9
Nat <nat@cmmc.io>
2017-10-19 03:33:31 +0700

    update

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@
     baudRate: parseInt(process.env.TARGET_BAUDRATE)
   });
 
-  port.on('open', () => { });
+  port.on('open', () => {});
 
   // open errors will be emitted as an error event
   port.on('error', (err) => {
@@ -16,8 +16,14 @@
   });
 
   port.on('data', (data) => {
-    console.log(data);
+    // console.log(data);
     // console.log(chalk.cyan('Data: ' + data));
+  });
+
+
+  const parser = port.pipe(new Delimiter({delimiter: Buffer.from('0d0a', 'hex')}));
+  parser.on('data', function (data) {
+    console.log(data);
   });
 
 }
