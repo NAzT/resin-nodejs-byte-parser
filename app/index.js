f98f283ab9244ceeaf302b10ea1721d8c4e3d8a5
Nat <nat@cmmc.io>
2017-10-19 02:45:41 +0700

    update

--- app/index.js
+++ app/index.js
@@ -8,39 +8,14 @@
     baudRate: parseInt(process.env.TARGET_BAUDRATE)
   });
 
-  SerialPort.list((err, ports) => {
-    'use strict';
-    console.log('\nList of serial interfaces:');
-    ports.forEach((port) => {
-      console.log(chalk.yellow(port.comName));
-      if (port.pnpId) {
-        console.log(chalk.yellow(port.pnpId));
-      }
-      if (port.manufacturer) {
-        console.log(chalk.yellow(port.manufacturer));
-      }
-    });
-    console.log('\n');
-  });
-
-  port.on('open', () => {
-    'use strict';
-    port.write(process.env.TEST_CMD, (err) => {
-      if (err) {
-        return console.log(chalk.red('Error on write: ', err.message));
-      }
-      console.log(chalk.magenta('message written'));
-    });
-  });
+  port.on('open', () => { });
 
   // open errors will be emitted as an error event
   port.on('error', (err) => {
-    'use strict';
-    console.log(chalk.red('Error: ', err.message));
+    // console.log(chalk.red('Error: ', err.message));
   });
 
   port.on('data', (data) => {
-    'use strict';
     console.log(data);
     // console.log(chalk.cyan('Data: ' + data));
   });
