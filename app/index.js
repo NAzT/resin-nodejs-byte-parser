4e62ee623f614810e4067a3bc0569b95e6a6a37e
Nat <nat@cmmc.io>
2017-10-19 03:35:43 +0700

    update

--- app/index.js
+++ app/index.js
@@ -1,29 +1,26 @@
 #!/bin/env node
 
-{
+const chalk = require('chalk');
+const SerialPort = require('serialport');
+const Delimiter = SerialPort.parsers.Delimiter;
+const port = new SerialPort(process.env.TARGET_PORT, {
+  baudRate: parseInt(process.env.TARGET_BAUDRATE)
+});
 
-  const chalk = require('chalk');
-  const SerialPort = require('serialport');
-  const port = new SerialPort(process.env.TARGET_PORT, {
-    baudRate: parseInt(process.env.TARGET_BAUDRATE)
-  });
+port.on('open', () => {});
 
-  port.on('open', () => {});
+// open errors will be emitted as an error event
+port.on('error', (err) => {
+  // console.log(chalk.red('Error: ', err.message));
+});
 
-  // open errors will be emitted as an error event
-  port.on('error', (err) => {
-    // console.log(chalk.red('Error: ', err.message));
-  });
-
-  port.on('data', (data) => {
-    // console.log(data);
-    // console.log(chalk.cyan('Data: ' + data));
-  });
+port.on('data', (data) => {
+  // console.log(data);
+  // console.log(chalk.cyan('Data: ' + data));
+});
 
 
-  const parser = port.pipe(new Delimiter({delimiter: Buffer.from('0d0a', 'hex')}));
-  parser.on('data', function (data) {
-    console.log(data);
-  });
-
-}
+const parser = port.pipe(new Delimiter({delimiter: Buffer.from('0d0a', 'hex')}));
+parser.on('data', function (data) {
+  console.log(data);
+});
