5f1c33853e493e176189109ac9cd0fbff2487b7d
Nat <nat@cmmc.io>
2017-10-19 15:25:04 +0700

    update

--- app/index.js
+++ app/index.js
@@ -31,8 +31,9 @@ const CMMCParser = new Parser().endianess('big')
     length: 2,
     formatter: toHexString
   })
+  .uint8('version')
+  .uint8('type')
   .uint32('reserved')
-  .uint16('type')
   .array('from', {
     type: 'uint8',
     length: 6,
